e3f41b2f3c3625e7bfde268ec5c613792dfe6368
Dipti Rani <gdipti642@gmail.com>
2022-11-03 12:21:08 +0530

    create models users and images

---
+++ src/models/images.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const Images = mongoose.model('Images', {
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    location: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        required: true,
+        default: new Date()
+    }
+})
+
+module.exports = Images;
---
+++ src/models/users.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const Users = mongoose.model('Users', {
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    points: {
+        type: Number,
+        default: 0 
+    }
+
+})
+
+module.exports = Users;
